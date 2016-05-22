52c77fad2b29a276fb0843b18e743f8fd551a14c
ripe72_hack <foo@bar>
2016-05-22 10:19:34 -0400

    Fix path

--- config/doctrine.php
+++ config/doctrine.php
@@ -34,7 +34,7 @@ return [
             'repository' => Doctrine\ORM\EntityRepository::class,
             'proxies'    => [
                 'namespace'     => false,
-                'path'          => database_path('proxies'),
+                'path'          => database_path('Proxies'),
                 'auto_generate' => env('DOCTRINE_PROXY_AUTOGENERATE', false)
             ],
             /*
