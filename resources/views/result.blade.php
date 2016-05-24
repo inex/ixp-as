3fa06df35c3233f8f35205fceed5362411ae4db1
Barry O'Donovan <barry@opensolutions.ie>
2016-05-25 01:55:39 +0200

    Refresh until done

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -11,7 +11,23 @@
     <li> <strong>Requested:</strong> {{ $request->created->format('Y-m-d H:i:s') }} </li>
     <li> <strong>Started:</strong> {{ $request->started ? $request->started->format('Y-m-d H:i:s') : 'Pending' }} </li>
     @if ($request->started)
-        <li> <strong>Completed:</strong> {{ $request->completed ? $request->completed->format('Y-m-d H:i:s') : 'Still running' }} </li>
+        <li> <strong>Completed:</strong>
+            @if ($request->completed)
+                {{ $request->completed->format('Y-m-d H:i:s') }}
+            @else
+                Still running. (page reloads in: <span id="seconds">60</span> seconds)
+
+                <script>
+                (function countdown(remaining) {
+                    if(remaining <= 0)
+                        location.reload(true);
+                    document.getElementById('seconds').innerHTML = remaining;
+                    setTimeout(function(){ countdown(remaining - 1); }, 1000);
+                })(60); // 60 seconds
+                </script>
+
+            @endif
+        </li>
     @endif
 </ul>
 
