78df5dcdcd95ea04859902629ed4026c043c1da8
Barry O'Donovan <barry@opensolutions.ie>
2016-05-26 15:16:36 +0200

    Do not show ugly nulls

--- public/js/result.js
+++ public/js/result.js
@@ -18,25 +18,34 @@ function showJSON( id ) {
 
     $('#modal-json').modal('hide');
 
+    body = function( data ) {
+        alert(data);
+        if( data == "null" ) {
+            return "<p>No data available yet...</p>";
+        } else {
+            return "<pre>" + data + "</pre>";
+        }
+    };
+
     switch( type ) {
         case 'requestin':
             $('#modal-json-header').html( "Request: " + (results.measurements[meas]).dnetwork.name + " to " + results.snetwork.name );
-            $('#modal-json-body').html( "<pre>" + (results.measurements[meas]).atlas_in_request_data + "</pre>");
+            $('#modal-json-body').html( body( (results.measurements[meas]).atlas_in_request_data ) );
             break;
 
         case 'requestout':
             $('#modal-json-header').html( "Request: " + results.snetwork.name + " to " + (results.measurements[meas]).dnetwork.name );
-            $('#modal-json-body').html( "<pre>" + (results.measurements[meas]).atlas_out_request_data + "</pre>");
+            $('#modal-json-body').html( body( (results.measurements[meas]).atlas_out_request_data ) );
             break;
 
         case 'responsein':
             $('#modal-json-header').html( "Response: " + (results.measurements[meas]).dnetwork.name + " to " + results.snetwork.name );
-            $('#modal-json-body').html( "<pre>" + (results.measurements[meas]).atlas_in_data + "</pre>");
+            $('#modal-json-body').html( body( (results.measurements[meas]).atlas_in_data ) );
             break;
 
         case 'responseout':
             $('#modal-json-header').html( "Response: " + results.snetwork.name + " to " + (results.measurements[meas]).dnetwork.name );
-            $('#modal-json-body').html( "<pre>" + (results.measurements[meas]).atlas_out_data + "</pre>");
+            $('#modal-json-body').html( body( (results.measurements[meas]).atlas_out_data ) );
             break;
 
 
